29d2da6d1028c88096c7bad060e8d67b0e5b7296
nikgonza <nikgonza@LES008296.corp.capgemini.com>
2023-02-02 11:12:23 +0100

    Cambiado metodo save()
    Ahora lanza un error en caso de que exista ya un cliente con el mismo
    nombre

--- src/main/java/com/ccsw/tutorial/client/ClientController.java
+++ src/main/java/com/ccsw/tutorial/client/ClientController.java
@@ -1,48 +1,52 @@
 package com.ccsw.tutorial.client;
 
+import com.ccsw.tutorial.client.model.ClientDto;
+import com.ccsw.tutorial.exception.ExistsException;
+import com.devonfw.module.beanmapping.common.api.BeanMapper;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.ccsw.tutorial.client.model.ClientDto;
-import com.ccsw.tutorial.exception.ExistsException;
-import com.devonfw.module.beanmapping.common.api.BeanMapper;
+import org.springframework.web.server.ResponseStatusException;
 
 @RequestMapping(value = "/clients")
 @RestController
 @CrossOrigin(origins = "*")
 public class ClientController {
+  @Autowired
+  ClientService clientService;
 
-    @Autowired
-    ClientService clientService;
+  @Autowired
+  BeanMapper beanMapper;
 
-    @Autowired
-    BeanMapper beanMapper;
+  /**
+   *
+   * @return
+   */
+  @RequestMapping(path = "", method = RequestMethod.GET)
+  public List<ClientDto> findAll() {
+    return this.beanMapper.mapList(clientService.findAll(), ClientDto.class);
+  }
 
-    /**
-     * 
-     * @return
-     */
-    @RequestMapping(path = "", method = RequestMethod.GET)
-    public List<ClientDto> findAll() {
-
-        return this.beanMapper.mapList(clientService.findAll(), ClientDto.class);
+  @RequestMapping(path = { "", "/{id}" }, method = RequestMethod.PUT)
+  public void save(
+    @PathVariable(name = "id", required = false) Long id,
+    @RequestBody ClientDto clientDto
+  ) {
+    try {
+      this.clientService.save(id, clientDto);
+    } catch (ExistsException ex) {
+      throw new ResponseStatusException(HttpStatus.BAD_GATEWAY, ex.getMessage());
     }
+  }
 
-    @RequestMapping(path = { "", "/{id}" }, method = RequestMethod.PUT)
-    public void save(@PathVariable(name = "id", required = false) Long id, @RequestBody ClientDto clientDto)
-            throws ExistsException {
-        this.clientService.save(id, clientDto);
-    }
-
-    @RequestMapping(path = "/{id}", method = RequestMethod.DELETE)
-    public void delete(@PathVariable(name = "id", required = false) Long id) {
-        this.clientService.delete(id);
-    }
+  @RequestMapping(path = "/{id}", method = RequestMethod.DELETE)
+  public void delete(@PathVariable(name = "id", required = false) Long id) {
+    this.clientService.delete(id);
+  }
 }
